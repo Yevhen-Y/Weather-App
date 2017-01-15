c89f7f12243f8aabf26e578ed21837c3102daff9
evan <text2evan@gmail.com>
2017-01-15 20:13:02 +0200

    find a foreast in your city

--- Weather App/ViewController.swift
+++ Weather App/ViewController.swift
@@ -8,8 +8,84 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var textField: UITextField!
+    
+    @IBOutlet var resultLabel: UILabel!
 
+    @IBAction func submitButton(_ sender: Any) {
+        
+        self.view.endEditing(true)
+
+        
+        if let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=" + textField.text!.replacingOccurrences(of: " ", with: "%20") + "&appid=de4ced2e2547330fcc10bc405dddc049") {
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            if error != nil {
+                
+                print(error!)
+                
+            } else {
+                
+                if let urlContent = data {
+                    
+                    do {
+                        
+                        let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        
+                        print(jsonResult)
+                        
+                        
+                        if let description = ((jsonResult["weather"] as? NSArray)?[0] as? NSDictionary)?["description"] as? String,
+                            let pressure = (jsonResult["main"] as? NSDictionary)?["pressure"] as? Float,
+                            let humidity = (jsonResult["main"] as? NSDictionary)?["humidity"] as? Float,
+                            let temp = (jsonResult["main"] as? NSDictionary)?["temp"] as? Float,
+                            let wind = (jsonResult["wind"] as? NSDictionary)?["speed"] as? Float
+                            {
+                            
+                            DispatchQueue.main.sync(execute: {
+                                print(temp)
+                                self.resultLabel.text = description + "\nTemp: " +
+                                    String( Int(temp - 273.0)) + "°C" + "\nPressure: " +
+                                    String(pressure) + "\nHumidity: " + String(humidity) + "\nWind speed: " + String(wind)
+                                
+                                
+                            })
+                            
+                        } else {
+                            
+                            DispatchQueue.main.sync(execute: {
+                                
+                                self.resultLabel.text = "Not found city"
+                                
+                            })
+                            
+                        }
+                        
+                        
+                    } catch {
+                        
+                        print("JSON Processing Failed")
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+        task.resume()
+            
+        } else {
+            
+            resultLabel.text = "Couldn't find that city - please try another"
+            
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +96,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+// some properties for keyboard
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
 
+    
 }
 
